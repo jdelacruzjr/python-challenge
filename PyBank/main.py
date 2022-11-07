b7a5b32a0034c4b2f9aaac8eba5ba970d408dc89
jdelacruzjr <115424617+jdelacruzjr@users.noreply.github.com>
2022-11-07 06:52:38 -0600

    Updated PyBank main.py file
    Backup working main.py file

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,40 +15,90 @@ total_months = 0
 # Counter for Total Profit & Loss
 total_profit_loss = 0
 
-# Counter to track value of Total Profit & Loss
+# Counter to track Value of Total Profit & Loss
 value = 0
 
-# Counter to track changes in Total Profit & Loss
+# Counter to track Changes in Total Profit & Loss
 change = 0
 
+# List to hold Dates of financial activity in dataset
+dates = []
 
+# List to hold Profits/Losses
+profits = []
 
-#----------------------------------------
-
-## EXAMPLE SYNTAX ONLY!!!! Write a function that returns the arithmetic average for a list of numbers
-def average(numbers):
-    length = len(numbers)
-    total = 0.0
-    for number in numbers:
-        total += number
-    return total / length
+# Read csv file
+with open(data_path, newline="") as budget_file:
+    csvreader = csv.reader(budget_file, delimiter=",")
     
+    # Read Header row
+    csv_header = next(csvreader)
     
-## EXAMPLE SYNTAX ONLY!!!! Create a .txt file and write to that .txt file
-
-# save the output file path (CREATE .TXT FILE)
-output_file = os.path.join("Analysis_Results.txt")
-
-# open the output file, create a header row, and then write the zipped object to the csv
-with open(output_file, "w") as datafile:
-    writer = csv.writer(datafile)
-
-    writer.writerow(["Index", "Employee", "Department"])
-
-    writer.writerow(["1", "Michael", "Boss"])
+    # Go to First row
+    first_row = next(csvreader)
     
-    writer.writerow(["2", "Dwight", "Sales"])
+    # Add Total Months counter
+    total_months += 1
+    
+    # Add Total Profit & Loss counter
+    total_profit_loss += int(first_row[1])
+    value = int(first_row[1])
+    
+    # Read Rows after Header row
+    for row in csvreader:
+    
+        # Get the Date
+        dates.append(row[0])
+        
+        # Track Changes of Rows
+        change = int(row[1])-value
+        profits.append(change)
+        value = int(row[1])
+        
+        # Total Month Count
+        total_months += 1
+        
+        # Calculate Net Total Profit & Loss Amount over entire period
+        total_profit_loss = total_profit_loss + int(row[1])
+        
+        # Calculate Average Change in Amount for Profit & Loss over entire period
+        avg_change = sum(profits)/len(profits)
+        
+    # Find Greatest Amount of Increase in Profit
+    greatest_increase = max(profits)
+    greatest_inc_index = profits.index(greatest_increase)
+    greatest_inc_date = dates[greatest_inc_index]
+        
+    # Find Greatest Amount of Decrease in Profit
+    greatest_decrease = min(profits)
+    greatest_dec_index = profits.index(greatest_decrease)
+    greatest_dec_date = dates[greatest_dec_index]
+        
+# Output Financial Analysis to Terminal
+printoutput = (
+    f"Financial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {str(total_months)}\n"
+    f"Total: ${str(total_profit_loss)}\n"
+    f"Average Change: ${str(round(avg_change,2))}\n"
+    f"Greatest Increase in Profits: {greatest_inc_date} (${str(greatest_increase)})\n"
+    f"Greatest Decrease in Profits: {greatest_dec_date} (${str(greatest_decrease)})\n")
+print(printoutput)
+        
+# Export Financial Analysis to Text File
+output_file = os.path.join('Analysis', 'Analysis_Results.txt')
+        
+Analysis_Results = open(output_file, "w")
+        
+line1 = "-----------------------"
+line2 = "Financial Analysis"
+line3 = "-----------------------"
+line4 = str(f"Total Months: {str(total_months)}")
+line5 = str(f"Total: ${str(total_profit_loss)}")
+line6 = str(f"Average Change: ${str(round(avg_change,2))}")
+line7 = str(f"Greatest Increase in Profits: {greatest_inc_date} (${str(greatest_increase)})")
+line8 = str(f"Greatest Decrease in Profits: {greatest_dec_date} (${str(greatest_decrease)})")
+Analysis_Results.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1, line2, line3, line4, line5, line6, line7, line8))
 
-    writer.writerow(["3", "Meredith", "Sales"])
-
-    writer.writerow(["4", "Kelly", "HR"])
+#-------------------
+#End Analysis
